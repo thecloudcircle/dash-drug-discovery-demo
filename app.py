7acaff2c186e9f6329a6ef28ebbeb3a48dedffa7
chriddyp <chris@plot.ly>
2018-02-22 12:22:31 -0500

    bump reqs and route through heroku

--- app.py
+++ app.py
@@ -6,9 +6,12 @@ import pandas as pd
 import flask
 import os
 
-server = flask.Flask('drug-discovery')
-app = dash.Dash('drug-discovery', server=server, url_base_pathname='/dash/gallery/drug-explorer/', csrf_protect=False)
-server.secret_key = os.environ.get('secret_key', 'secret')
+app = dash.Dash(
+    'drug-discovery',
+    url_base_pathname='/dash/gallery/drug-explorer/'
+)
+server = app.server
+
 
 df = pd.read_csv('small_molecule_drugbank.csv').drop(['Unnamed: 0'],axis=1)
 
@@ -16,6 +19,8 @@ if 'DYNO' in os.environ:
     app.scripts.append_script({
         'external_url': 'https://cdn.rawgit.com/chriddyp/ca0d8f02a1659981a0ea7f013a378bbd/raw/e79f3f789517deec58f41251f7dbb6bee72c44ab/plotly_ga.js'
     })
+    app.config.routes_pathname_prefix = '/dash/gallery/drug-explorer/'
+    app.config.requests_pathname_prefix = 'https://dash-drug-discovery-demo.herokuapp.com/dash/gallery/drug-explorer/'
 
 
 def add_markers( figure_data, molecules, plot_type = 'scatter3d' ):
